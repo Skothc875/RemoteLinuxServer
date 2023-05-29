2783f1d964e66e2bc2dd9bd93136ac90ee15b1f5
Skothc875 <129518898+Skothc875@users.noreply.github.com>
2023-05-29 19:05:50 +0700

    Update TLS_configurator.py
    Changing the hashing algorithm.

--- TLS_configurator.py
+++ TLS_configurator.py
@@ -7,7 +7,7 @@ except:
 token = input("Введите токен который будет использоваться в боте: ")
 passwd = input("Введите пароль который будет использоваться в боте: ")
 conffile = open('config.py', 'w', encoding='utf-8')
-conffile.write("hashpass = '" + hashlib.sha256(passwd.encode('utf-8')).hexdigest() + "'\n")
+conffile.write("hashpass = '" + hashlib.sha512(passwd.encode('utf-8')).hexdigest() + "'\n")
 conffile.write("token = '" + token + "'\n")
 print("Файл успешно сконфигурирован")
         
