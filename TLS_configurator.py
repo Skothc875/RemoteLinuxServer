973d60d86c3f76a32c978b4f39bf7664c71cfb9b
Skothc875 <129518898+Skothc875@users.noreply.github.com>
2023-06-02 13:08:15 +0700

    Update TLS_configurator.py
    Version 1.2

--- TLS_configurator.py
+++ TLS_configurator.py
@@ -1,14 +1,44 @@
 import hashlib
 import subprocess
+cho_lang = True
 try:
     import telebot
 except:
     subprocess.call("pip install pyTelegramBotAPI", shell=True)
-token = input("Введите токен который будет использоваться в боте: ")
-passwd = input("Введите пароль который будет использоваться в боте: ")
-conffile = open('config.py', 'w', encoding='utf-8')
-conffile.write("hashpass = '" + hashlib.sha512(passwd.encode('utf-8')).hexdigest() + "'\n")
-conffile.write("token = '" + token + "'\n")
-print("Файл успешно сконфигурирован")
-        
+print("1) English")
+print("2) Russian")
+print("3) Spanish")
+print("4) French")
+print("5) German")
+language = int(input("Choose your language: "))
+while cho_lang:
+    if language == 1:
+        import languages_folder.EN_lang as lan
+        cho_lang = False
+    
+    elif language == 2:
+        import languages_folder.RU_lang as lan
+        cho_lang = False 
 
+    elif language == 3:
+        import languages_folder.ES_lang as lan
+        cho_lang = False 
+
+    elif language == 4:
+        import languages_folder.FR_lang as lan
+        cho_lang = False 
+
+    elif language == 5:
+        import languages_folder.DE_lang as lan
+        cho_lang = False 
+
+    else:
+        print("You have chosen the wrong language.")
+
+token = input(lan.enter_token)
+passwd = input(lan.enter_passwd_set)
+conffile = open('config.py', 'w', encoding='utf-8')
+conffile.write("lang = " + str(language) + "\n")
+conffile.write("hashpass = '" + (hashlib.sha512(passwd.encode('utf-8')).hexdigest() + "'\n")
+conffile.write("token = '" + token + "'\n")
+print(lan.file_configured)
