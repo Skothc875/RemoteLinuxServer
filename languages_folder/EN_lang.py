d4269204d413f5ad90e3413c252a4a2bb745ed94
Skothc875 <129518898+Skothc875@users.noreply.github.com>
2023-06-02 13:06:00 +0700

    Language Packs

---
+++ languages_folder/EN_lang.py
@@ -0,0 +1,29 @@
+# Английскийязыковой пакет
+started = "[+]TelegaServer started"
+enter_token = "Enter the token to be used in the bot: "
+enter_passwd_set = "Enter the password to be used in the bot: "
+file_configured = "File configured successfully"
+press_c = "  PRESS CTRL+C TO EXIT                                                                                        "
+fail_started = "Failed to start bot :("
+welcome = "Welcome, {0.first_name}!\nI am your computer.\n/auth - authentication on the server\n/system - accesses the server.\nSee the documentation for more details about bot commands."
+enter_passwd = "Enter the password from the server to authenticate."
+auth_success = "Authentication was successful."
+wrong_pass = "Wrong password"
+enter_comm = "Enter command."
+no_access = "You don't have access."
+select_file_down = "Select the file you want to download from the given directory."
+select_file_up = "Select the file you want to upload to this directory."
+err_comm = "An error occurred while executing the command. Error code: "
+err_cd = "There was an error changing the directory, you may not have specified the correct path."
+err_sand_file = "Error sending file, please check file name."
+file_save_in = "The file was successfully saved in the directory"
+disconnect = "Disconnecting from server..."
+err_init = "Error initializing bot."
+
+
+
+
+
+
+
+
